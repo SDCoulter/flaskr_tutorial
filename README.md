b22e2a1cef52c4fe3b4c8f3d04c0813d4409eb42
Shaun Coulter <shaundcoulter@gmail.com>
2021-11-11 12:43:37 -0400

    create __init__ app file (flask tut)

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Formula 1 Web App / Data Analysis
 
 Repo for creating a web app (visualizations, etc), and data analysis. The data has been pulled from the [Ergast Developer API](http://ergast.com/mrd/).
+
+## Recording Goals
+
+* Create an 'app' to display the constructor's championship standings in 2020.
---
+++ flaskr/__int__.py
@@ -0,0 +1,45 @@
+"""
+Initial setup from Flask tutorial.
+https://flask.palletsprojects.com/en/2.0.x/tutorial/factory/
+"""
+
+# Dependencies.
+import os
+from flask import Flask
+
+# Factory function.
+def create_app(test_config=None):
+    # Create Flask instance - files relative to instance folder.
+    # Outside flaskr/ folder.
+    app = Flask(__name__, instance_relative_config=True)
+
+    # Sets default config that the app will use.
+    app.config.from_mapping(
+        # Override with random value before deployment.
+        SECRET_KEY='dev',
+        # Choose the database file/location.
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite')
+    )
+
+    if test_config is None:
+        # Overrides the default config with values from config.py file - if it exists.
+        # Can be used to set a real SECRET_KEY.
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # Load the test config if passed in.
+        # Configured independently with any development values.
+        app.config.from_mapping(test_config)
+
+    # Ensure the instance folder exists.
+    # Not done automatically by FLask - needed for SQLite database.
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # Create a simple route to see application function.
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    return app
